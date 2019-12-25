83d0f44e3811bc50d2e6426a0240336ee4eb5bb6
unknown <radkevich.viktor83@gmail.com>
2019-12-25 11:33:59 +0300

    add nodemon & other

--- mainApp/routes/index.js
+++ mainApp/routes/index.js
@@ -3,7 +3,29 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const posts = [
+    {
+        img: 'https://cdn2.awesound.com/images/user_michael.russo/2019/27f05405-6068-441e-9acd-54d612a3893d/The_Motivation_Movement_Cover_Photo.png',
+        title: 'Post 1',
+        small: 'So I practice turning people into trees. Which means appreciating them just the way they are. 1',
+    },
+    {
+        img: 'https://cdn2.awesound.com/images/user_michael.russo/2019/27f05405-6068-441e-9acd-54d612a3893d/The_Motivation_Movement_Cover_Photo.png',
+        title: 'Post 2',
+        small: 'So I practice turning people into trees. Which means appreciating them just the way they are.asdqweqsda 2',
+    },
+    {
+        img: 'https://cdn2.awesound.com/images/user_michael.russo/2019/27f05405-6068-441e-9acd-54d612a3893d/The_Motivation_Movement_Cover_Photo.png',
+        title: 'Post 3',
+        small: 'So I practice turning people into trees. Which means appreciating them just the way they are.zzzzzzzzzzzz 3',
+    },
+    {
+        img: 'https://cdn2.awesound.com/images/user_michael.russo/2019/27f05405-6068-441e-9acd-54d612a3893d/The_Motivation_Movement_Cover_Photo.png',
+        title: 'Post 4',
+        small: 'So I practice turning people into trees. Which means appreciating them just the way they are.qqqqqqqqqqqqqq 4',
+    }
+];
+  res.render('index', { title: 'Express',posts});
 });
 
 module.exports = router;
